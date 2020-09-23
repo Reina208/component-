cd815a92f7e7c81d647479ff04e64c17c1bf458f
Reina208 <70763130+Reina208@users.noreply.github.com>
2020-09-23 10:48:04 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <Header @getChildText="showChildText"></Header>
+    <p>{{text}}</p>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    showChildText(childText) {
+      this.text = childText;
+    }
+  },
+  components: {
+    Header
+  }
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="side__menu">
+    <input type="text" v-model="childText" />
+    <button @click="sendParent">親に送る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      childText: ""
+    };
+  },
+  methods: {
+    sendParent() {
+      this.$emit("getChildText", this.childText);
+    }
+  }
+};
+</script>
